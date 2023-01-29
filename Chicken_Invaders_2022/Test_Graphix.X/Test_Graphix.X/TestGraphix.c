e404271ed6827e2831c001cc9be04f589fb4d2d2
yuvalBerghaus <yuvalBerghaus>
2023-01-29 16:14:55 +0200

    working on the fucking i2c made a function with the flow of reading one byte from the slave (in our case accelarometer)

--- Chicken_Invaders_2022/Test_Graphix.X/Test_Graphix.X/TestGraphix.c
+++ Chicken_Invaders_2022/Test_Graphix.X/Test_Graphix.X/TestGraphix.c
@@ -13,12 +13,33 @@
 #include "oledDriver/oledC.h"
 #include "oledDriver/oledC_colors.h"
 #include "oledDriver/oledC_shapes.h"
-
+#include "i2c1_driver/i2c1_driver.h"
 int flag = false; // global variable flag that represents the transition from screen_2 to the game when it becomes true
 uint8_t jump_down = 10;
 uint8_t current_start_y_enemy = 0;
 uint8_t current_end_y_enemy = 10;
 //The following variable will save the location of the player so whenever i clear the screen 
+
+
+char* itoa(int i, char b[]){
+    char const digit[] = "0123456789";
+    char* p = b;
+    if(i<0){
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do{ //Move to where representation ends
+        ++p;
+        shifter = shifter/10;
+    }while(shifter);
+    *p = '\0';
+    do{ //Move back, inserting digits as u go
+        *--p = digit[i%10];
+        i = i/10;
+    }while(i);
+    return b;
+}
 
 
 void display_screen1() {
@@ -35,6 +56,53 @@ void display_screen2() {
       oledC_DrawString(25, 35, 2, 2, "Wars", OLEDC_COLOR_WHITE); 
       oledC_DrawString(5, 65, 1, 1, "Press any key", OLEDC_COLOR_WHITE); 
       oledC_DrawString(22, 80, 1, 1, "to begin", OLEDC_COLOR_WHITE); 
+}
+
+void start_reading() {
+    i2c1_driver_driver_open();
+    i2c1_driver_start();
+    i2c1_driver_TXData(0x3A); // send slave address and write bit
+    i2c1_driver_waitRX();
+    if (i2c1_driver_isNACK()) {
+        oledC_DrawString(5, 65, 1, 1, "Error: NACK received", OLEDC_COLOR_WHITE);
+        i2c1_driver_stop();
+        return 1;
+    }
+    
+    // send data to slave device
+    i2c1_driver_TXData(0x31);
+    i2c1_driver_waitRX();
+    if (i2c1_driver_isNACK()) {
+        oledC_DrawString(5, 65, 1, 1, "Error: NACK received", OLEDC_COLOR_WHITE);
+        i2c1_driver_stop();
+        return 1;
+    }
+    
+    i2c1_driver_stop();
+    i2c1_driver_start();
+    i2c1_driver_TXData(0x3B); // send slave address and read bit
+    i2c1_driver_waitRX();
+    if (i2c1_driver_isNACK()) {
+        oledC_DrawString(5, 65, 1, 1, "Error: NACK received", OLEDC_COLOR_WHITE);
+        i2c1_driver_stop();
+        return 1;
+    }
+    
+    // receive data from slave device
+    i2c1_driver_startRX();
+    i2c1_driver_waitRX();
+    char data = i2c1_driver_getRXData();
+//    printf("Data received from slave: %d\n", data);
+    
+    ///////////////////////////////////////////////////////////////
+    char buffer_s1[33];
+    char * information = itoa(data,buffer_s1);
+    oledC_DrawString(20, 75, 1, 1, information, OLEDC_COLOR_WHITE); 
+    //////////////////////////////////////////////////////////////
+    i2c1_driver_sendNACK();
+    i2c1_driver_stop();
+    
+    i2c1_driver_driver_close();
 }
 
 void start_game() {
@@ -69,9 +137,7 @@ void Init() {
     IPC0bits.T1IP=1;
     IEC0bits.T1IE=1;
     INTCON2bits.GIE=1;
-    
-    
-    
+
 }
 
 
@@ -89,6 +155,7 @@ void __attribute__((__interrupt__)) _T1Interrupt(void)
         current_start_y_enemy += jump_down;
         current_end_y_enemy += jump_down;
     }
+    start_reading();
     IFS0bits.T1IF=0;
 }
 
